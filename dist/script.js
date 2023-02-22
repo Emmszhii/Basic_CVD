0706338c62c9ae0cfe6d863398591ef2737855f7
unknown <emmanuelmanalo707@gmail.com>
2023-02-22 15:13:45 +0800

    about html

--- dist/script.js
+++ dist/script.js
@@ -8,29 +8,29 @@ toggleMenu.addEventListener("click", () => {
 
 
 
-const header = document.querySelector('.header');
+// const header = document.querySelector('.header');
 
-const servicesObserver = () =>{
-  const option = {
-    root: null,
-    rootMargin: '20px',
-    threshold: 1.0
-  }
+// const servicesObserver = () =>{
+//   const option = {
+//     root: null,
+//     rootMargin: '20px',
+//     threshold: 1.0
+//   }
   
-  const cb = (entry) =>{
-    entry.map(element => {
-      if(element.isIntersecting){
-        header.style.backgroundColor = '#ffffff'
-      }else{
-        header.style.backgroundColor = '#2b2b2b'
-      }
-    });
-  }
+//   const cb = (entry) =>{
+//     entry.map(element => {
+//       if(element.isIntersecting){
+//         header.style.backgroundColor = '#ffffff'
+//       }else{
+//         header.style.backgroundColor = '#2b2b2b'
+//       }
+//     });
+//   }
   
-  const observer = new IntersectionObserver(cb, option);
-  observer.observe(hmServices);
-}
+//   const observer = new IntersectionObserver(cb, option);
+//   observer.observe(hmServices);
+// }
 
-window.addEventListener('DOMContentLoaded', ()=>{
-  servicesObserver();
-});
+// window.addEventListener('DOMContentLoaded', ()=>{
+//   servicesObserver();
+// });
